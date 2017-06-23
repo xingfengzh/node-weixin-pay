1243ffcdd3837bf1bf311116649ab58ca2c08b38
xingfengzh <xingfengzh_candy@163.com>
2017-06-23 02:58:01 -0500

    node 微信

--- README.md
+++ README.md
@@ -1,1 +1,172 @@
-# node-weixin-pay
+#  [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coveralls Status][coveralls-image]][coveralls-url]
+
+> Weixin Pay functions for node
+
+
+支持QQ群：39287176
+
+修改了部分参数校验的规则
+
+## Install
+
+```sh
+$ npm install --save node-weixin-pay
+```
+
+
+## Usage
+
+
+> 通用功能
+
+1、初始化对象与基本数据
+
+```js
+var nodeWeixinPay = require('node-weixin-pay');
+var nodeWeixinConfig = require('node-weixin-config');
+
+
+var merchant = {
+  id: process.env.MERCHANT_ID || 'id',
+  key: process.env.MERCHANT_KEY || 'key'
+};
+var app = {
+  id: process.env.APP_ID || 'appid',
+  secret: process.env.APP_SECRET || 'appsecret',
+  token: process.env.APP_TOKEN || 'apptoken'
+};
+
+var certificate = {
+  pkcs12: path.resolve(certPKCS12File),             //格式是文件名
+  key: String(certKey)
+};
+
+//或者
+
+var certificate = {
+  pfx: new Buffer(conf.merchant_pfx, 'base64'),     //格式是文件二进制内容
+  pfxKey: conf.merchant_id
+};
+
+//对于大部分的支付接口来说是需要config的
+var config = {
+  app: app,
+  merchant: merchant,
+  certificate: certificate
+};
+
+//校验数据的正确性
+nodeWeixinConfig.app.init(app);
+nodeWeixinConfig.merchant.init(merchant);
+
+var params = { openid: process.env.OPENID,
+  spbill_create_ip: '1.202.241.25',
+  notify_url: 'http://wx.domain.com/weixin/pay/main',
+  body: '测试支付',
+  out_trade_no: '111',
+  total_fee: '1',
+  trade_type: 'JSAPI',
+  appid: app.id,
+  mch_id: merchant.id,
+  nonce_str: 'XjUw56N8MjeCUqHCwqgiKwr2CJVgYUpe' };
+  
+```
+
+2、签名一个请求
+
+```js
+
+var sign = nodeWeixinPay.sign(merchant, params);
+```
+
+
+3、准备一个支付配置
+
+```js
+var id = 'id';
+var config = nodeWeixinPay.prepay(app, merchant, id);
+```
+
+
+> 具体的API请求部分
+
+4、发送统一支付请求
+
+```js
+nodeWeixinPay.api.order.unified(config, params, function(error, data) {
+});
+```
+
+5、发送订单查询请求
+
+```js
+var config = nodeWeixinPay.api.query(config, params, function(error, data) {
+});
+```
+
+6、发送订单关闭请求
+
+```js
+var config = nodeWeixinPay.api.close(config, params, function(error, data) {
+});
+```
+
+7、发送创建退款请求
+
+```js
+var config = nodeWeixinPay.refund.create(config, params, function(error, data) {
+});
+```
+
+8、发送退款查询请求
+
+```js
+var config = nodeWeixinPay.refund.query(config, params, function(error, data) {
+});
+```
+
+9、发送下载对账单请求
+
+```js
+var config = nodeWeixinPay.statements(config, params, function(error, data) {
+});
+```
+
+10、发送测速报告请求
+
+```js
+var config = nodeWeixinPay.report(config, params, function(error, data) {
+});
+```
+
+> 处理微信回调
+
+10、外理回调数据
+
+```js
+//req.rawBody should be enabled
+var req = {rawBody: xml};
+var res = {
+  json: function() {
+
+  }
+};
+nodeWeixinPay.callback.notify(app, merchant, req, res, function(error, data) {
+});
+```
+
+
+## License
+
+MIT © [node-weixin](www.node-weixin.com)
+
+
+[npm-image]: https://badge.fury.io/js/node-weixin-pay.svg
+[npm-url]: https://npmjs.org/package/node-weixin-pay
+[travis-image]: https://travis-ci.org/node-weixin/node-weixin-pay.svg?branch=master
+[travis-url]: https://travis-ci.org/node-weixin/node-weixin-pay
+[daviddm-image]: https://david-dm.org/node-weixin/node-weixin-pay.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/node-weixin/node-weixin-pay
+[coveralls-image]: https://coveralls.io/repos/node-weixin/node-weixin-pay/badge.svg?branch=master&service=github
+[coveralls-url]: https://coveralls.io/github/node-weixin/node-weixin-pay?branch=master
+
---
+++ index.js
@@ -0,0 +1,178 @@
+'use strict';
+
+/**
+ * For Weixin Pay ver 3.3.7
+ * @type {*|exports|module.exports}
+ */
+
+var _ = require('lodash');
+var request = require("node-weixin-request");
+var util = require("node-weixin-util");
+var v = require('node-form-validator');
+var errors = require('web-errors').errors;
+var crypto = require('crypto');
+var buffer = require('buffer');
+
+var pay = {
+  callback: require('./lib/callback'),
+  api:require('./lib/api'),
+  /**
+   * Handler for weixin server response
+   *
+   * @param app
+   * @param merchant
+   * @param json                    Validation for data received
+   * @param resultValidator         Validation for data result
+   * @param cb
+   * @returns {*}
+   */
+  handle: function (app, merchant, json, resultValidator, cb) {
+    var returnCode = json.return_code;
+    var returnMsg = json.return_msg;
+    var error = {};
+
+    if (returnCode === 'SUCCESS') {
+      var vError = this.validate(app, merchant, json);
+      if (true !== vError) {
+        return cb(true, vError, json);
+      }
+
+      //是否还要验证数据
+      if (resultValidator === null) {
+        return cb(false, null, json);
+      }
+      var resultCode = json.result_code;
+      if (resultCode === 'SUCCESS') {
+        if (!v.validate(json, resultValidator, error)) {
+          cb(true, error, json);
+          return;
+        }
+        var result = v.json.extract(json, resultValidator);
+        cb(false, result, json);
+        return;
+      }
+    }
+    cb(true, returnMsg, json);
+  },
+
+  /**
+   * Basic http request wrapper for pay apis, which need to be encrypted and verified for their data format
+   *
+   * @param url                 Requesting url
+   * @param data                Data to be sent
+   * @param sendConfig          Sending data validation configuration
+   * @param receiveConfig       Receiving data validation configuration
+   * @param certificate         Certificate from Tencent Pay
+   * @param cb                  Callback Function
+   */
+  request: function (config, url, data, sendConfig, receiveConfig, cb) {
+    var error = {};
+
+    //Validate Sending Data
+    if (!v.validate(data, sendConfig, error)) {
+      cb(true, error);
+      return;
+    }
+
+    var params = _.clone(data);
+    params = pay.prepare(config.app, config.merchant, params);
+    var sign = pay.sign(config.merchant, params);
+
+    params.sign = sign;
+    var xml = util.toXml(params);
+    request.xmlssl(url, xml, config.certificate, function (error, json) {
+      pay.handle(config.app, config.merchant, json, receiveConfig, cb);
+    });
+  },
+
+  /**
+   * Prepare data with normal fields
+   *
+   * @param data
+   * @param app
+   * @param merchant
+   * @param device
+   * @returns {*}
+   */
+  prepare: function (app, merchant, data, device) {
+    data.appid = app.id;
+    data.mch_id = merchant.id;
+    if (device) {
+      data.device_info = device.info;
+    }
+    data.nonce_str = util.getNonce();
+    return data;
+  },
+
+  /**
+   * Sign all data with merchant key
+   *
+   * @param merchant
+   * @param params
+   * @returns {string}
+   */
+  sign: function (merchant, params) {
+    var temp = util.marshall(params);
+    temp += '&key=' + String(merchant.key);
+    // temp = new buffer.Buffer(temp);
+    // temp = temp.toString("binary");
+    var crypt = crypto.createHash('MD5');
+    crypt.update(temp);
+    return crypt.digest('hex').toUpperCase();
+  },
+
+  /**
+   *  Validate header for data received
+   *
+   * @param data
+   * @param app
+   * @param merchant
+   * @returns {*}
+   */
+  validate: function (app, merchant, data, error) {
+    var config = require('./conf/validation');
+    var conf = config.auth.header;
+    error = error || {};
+
+    if (!v.validate(data, conf, error)) {
+      return errors.ERROR;
+    }
+    if (String(data.appid) !== String(app.id)) {
+      return errors.APP_ID_ERROR;
+    }
+    if (String(data.mch_id) !== String(merchant.id)) {
+      return errors.MERCHANT_ID_ERROR;
+    }
+    return true;
+  },
+
+  /**
+   *  Make prepay data for jssdk
+   *
+   * @param app
+   * @param merchant
+   * @param prepayId
+   * @returns {{appId: *, timeStamp: string, nonceStr, package: string, signType: string}}
+   */
+  prepay: function (app, merchant, prepayId) {
+    var crypto = require('crypto');
+    var md5 = crypto.createHash('md5');
+    var timeStamp = String(new Date().getTime());
+
+    md5.update(timeStamp);
+    timeStamp = Math.floor(timeStamp / 1000);
+
+    var nonceStr = md5.digest('hex');
+    var data = {
+      appId: app.id,
+      timeStamp: String(timeStamp),
+      nonceStr: nonceStr,
+      package: 'prepay_id=' + prepayId,
+      signType: 'MD5'
+    };
+    data.paySign = this.sign(merchant, data);
+    return data;
+  }
+};
+
+module.exports = pay;
